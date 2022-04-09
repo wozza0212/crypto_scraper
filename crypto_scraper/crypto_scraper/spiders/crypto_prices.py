053e2e01f272bf5080c0877c7d4f326332f25817
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2022-04-09 13:03:44 +0100

    crawler looking finished, need to test

--- crypto_scraper/crypto_scraper/spiders/crypto_prices.py
+++ crypto_scraper/crypto_scraper/spiders/crypto_prices.py
@@ -14,4 +14,16 @@ class CryptoPricesSpider(CrawlSpider):
         # Set restrict_xpaths to the xpath or the links you want to follow
 
     def parse_item(self, response):
-        print(response.url)
+        row = response.xpath("//tr[@class='cmc-table-row']")
+        # This row, is the xpath to a row of the table that we want to access
+
+        yield {
+            'Date': row.path("./h1/text()").get(),
+            'Name': row.xpath("./td/div/a[@class='cmc-table__column-name--name cmc-link']/text()").get(),
+            'Symbol': row.xpath("./td/div/a[@class='cmc-table__column-name--symbol cmc-link']/text()").get(),
+            'Price': row.xpath("./td[@class='cmc-table__cell cmc-table__cell--sortable cmc-table__cell--rightcmc-table__cell--sort-by__price']/div/text()").get(),
+            'Market Cap': row.xpath("./td[@class='cmc-table__cell cmc-table__cell--sortable cmc-table__cell--right cmc-table__cell--sort-by__market-cap']/text()").get(),
+            'Circulating Supply': row.xpath("./td[@class='cmc-table__cell cmc-table__cell--sortable cmc-table__cell--right cmc-table__cell--sort-by__circulating-supply']/text()").get(),
+            'url': response.url,
+
+        }
